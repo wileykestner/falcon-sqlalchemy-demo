044f87b77fcebeba04f4c92484a88f152654d3f7
Wiley Kestner <wiley@aya.yale.edu>
2016-10-17 08:50:00 -0700

    Minor formatting

--- test/test_people_DELETE.py
+++ test/test_people_DELETE.py
@@ -3,15 +3,16 @@ from test.utils.helpers import get_header_value, get_identifier_for_created_pers
 
 # noinspection PyPep8Naming
 class Test_Deleting_People(object):
+
     def test_status_code(self, create_person, delete_person):
-        create_response = create_person("Batman")
+        create_response = create_person('Batman')
         person_id = get_identifier_for_created_person(create_response)
         delete_response = delete_person(person_id)
 
         assert delete_response.status_code == 204
 
     def test_getting_after_deleting(self, create_person, application, delete_person):
-        create_response = create_person("Batgirl")
+        create_response = create_person('Batgirl')
         location = get_header_value('location', create_response.headers)
         person_id = get_identifier_for_created_person(create_response)
         delete_person(person_id)
