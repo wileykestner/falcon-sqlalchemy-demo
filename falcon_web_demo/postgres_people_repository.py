d07437914109545c265380e76ba4f9f2d1ec1f88
Wiley Kestner <wiley@aya.yale.edu>
2016-10-18 14:23:08 -0700

    Add isolated transaction management via private method in People Repository

--- falcon_web_demo/postgres_people_repository.py
+++ falcon_web_demo/postgres_people_repository.py
@@ -45,30 +45,27 @@ class PostgresPeopleRepository(PeopleRepository):
             return Person(identifier=identifier, name=name)
 
     def create_person(self, name: str) -> int:
-        session = self._session_provider.get_session()
-        postgres_person = PostgresPerson(name=name)
-        session.add(postgres_person)
-        session.commit()
-        identifier = postgres_person.id
-        session.close()  # untested
-        return identifier
+        with self._isolated_transaction(commit_on_exit=False) as session:
+            postgres_person = PostgresPerson(name=name)
+            session.add(postgres_person)
+            session.commit()
+            return postgres_person.id
 
     def fetch_people(self) -> Sequence[Person]:
-        session = self._session_provider.get_session()
-        person_list = [Person(identifier=p.id, name=p.name) for p in session.query(PostgresPerson, PostgresPerson.id,
-                                                                                   PostgresPerson.name).all()]
-        session.close()  # untested
-
-        return person_list
+        with self._isolated_transaction(commit_on_exit=False) as session:
+            return [Person(identifier=p.id, name=p.name) for p in session.query(PostgresPerson, PostgresPerson.id,
+                                                                                PostgresPerson.name).all()]
 
     @contextmanager
-    def _isolated_transaction(self):
+    def _isolated_transaction(self, commit_on_exit=True):
         session = self._session_provider.get_session()
         try:
             yield session
-            session.commit()
         except:
             session.rollback()
             raise
+        else:
+            if commit_on_exit:
+                session.commit()
         finally:
             session.close()
